458c6e805390595a96fcdc372a58879fe597b467
llebpmacds <david.scott.campbell@gmail.com>
2025-12-10 22:57:11 +0000

    Update configuration of db and cache

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
+// Uncommented the lines
 if(builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
